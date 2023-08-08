0b786da822f8cfd0af94cae53017fb4b14e3a47f
DianFenMian <110230921+DianFenMian@users.noreply.github.com>
2023-08-09 02:04:57 +0800

    Docs Ver0.1 始提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+import { gitPlugin } from "@vuepress/plugin-git";
+import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
+import { copyCodePlugin } from "vuepress-plugin-copy-code2";
+//import { pwaPlugin } from "@vuepress/plugin-pwa";
+//import { defaultTheme } from '@vuepress/theme-default'
+import themeSidebar from 'vuepress-theme-sidebar';
+
+export default {
+  title: '喵之梦 文档',
+  description: 'Welcome to Neko Docs, where the documents are put out.',
+  theme: themeSidebar({
+    sidebar: 'auto',
+    nextLinks: true,
+    prevLinks: true,
+    repo: 'DianFenMian/dianfenmian.github.io',
+    repoLabel: 'View on Github',
+    docsRepo: 'DianFenMian/dianfenmian.github.io',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLinks: true,
+    editLinkText: 'Edit on GitHub',
+    colorModeSwitch: true
+  }),
+  plugins: [
+    gitPlugin({
+      createdTime: false,
+      updatedTime: true,
+      contributors: true
+    }),
+    mdEnhancePlugin({
+      gfm: true,
+      container: true,
+      tabs: true,
+      codetabs: true,
+      card: true
+    }),
+    copyCodePlugin({
+      
+    })
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /Logos/单白.png
+heroText: 喵之梦 官方文档
+tagline: 我们的记忆，就是梦幻
+sidebar: true
+actions:
+  - text: 查看文档
+    type: primary 
+    link: /文档/
+  - text: 加入Q群
+    type: secondary
+    link: https://qm.qq.com/cgi-bin/qm/qr?k=yz2e97_MXHJrOzxz2NBXdlWZb27ZtyBM&jump_from=webapi&authKey=P+y1ZaFeufcniM28pN9jSQ72OHPvRNHASM2BhTgClwp8Fo04caPv/4lGNHSZwcH8
+
+footer: MIT Licensed | Copyright © 2023-2025 淀粉工坊
+---
